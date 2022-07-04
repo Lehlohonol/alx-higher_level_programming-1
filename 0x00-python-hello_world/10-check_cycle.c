723e4026029f0a698bfab016868aa19c93f7f9ff
IanoNjuguna <ichege438@gmail.com>
2022-07-05 00:03:13 +0300

    ALX-PYTHON

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,43 +1,32 @@
+#include <stdio.h>
 #include "lists.h"
 
 /**
- * check_cycle - checks if a singly linked list has
- * a cycle in it
- * @list: pointer to the list
- * Return: 0 if there is no cycle,
- * 1 if there is a cycle
- */
+* check_cycle - checks if a singly linked list has a cycle within.
+* @list: singly linked list.
+* Return: 0 is there is no cycle, 1 if there is a cycle.
+*/
+
 int check_cycle(listint_t *list)
 {
-listint_t *p2;
-listint_t *prev;
+        listint_t *head;
+        listint_t *tail;
 
-p2 = list;
-prev = list;
-while (list && p2 && p2->next)
-{
-list = list->next;
-p2 = p2->next->next;
+        if (list == NULL)
+                return (0);
 
-if (list == p2)
-{
-list = prev;
-prev =  p2;
-while (1)
-{
-p2 = prev;
-while (p2->next != list && p2->next != prev)
-{
-p2 = p2->next;
-}
-if (p2->next == list)
-break;
+        head = list;
+        tail = list;
 
-list = list->next;
-}
-return (1);
-}
+        while (tail != NULL && tail->next != NULL)
+        {
+                head = head->next;
+                tail = tail->next->next;
+
+                if (head == tail)
+                        return (1);
+
+        }
+        return (0);
 }
 
-return (0);
-}
--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-def print_last_digit(number):
-    if number >= 0:
-        l_digit = number % 10
-    else:
-        l_digit = number % -10
-        l_digit *= -1
 
-    print("{:d}".format(l_digit), end='')
-    return (l_digit)
+
+def print_last_digit(number):
+    if number < 0:
+        number = ((-1) * number) % 10
+    else:
+        number = number % 10
+
+    print(f"{number}", end="")
+    return number
